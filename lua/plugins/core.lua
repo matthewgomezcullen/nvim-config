44dad4cf1712848897ae1e014364b8b113e38c52
Matthew Gomez Cullen <mattgc1282@hotmail.co.uk>
2026-04-21 18:33:30 +0100

    Initial nvim config

---
+++ lua/plugins/core.lua
@@ -0,0 +1,48 @@
+return {
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup {}
+        end,
+    },
+    {
+        'stevearc/conform.nvim',
+        opts = {},
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    },
+    {
+        'nvim-telescope/telescope.nvim', version = '*',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            -- optional but recommended
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        }
+    },
+    { "neovim/nvim-lspconfig" },
+    {
+        "mason-org/mason.nvim",
+        opts = {}
+    }
+}
