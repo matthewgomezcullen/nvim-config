836ed4d0f687183cb90844bbb351b447b80d3db1
Matthew Gomez Cullen <101350485+matthewgomezcullen@users.noreply.github.com>
2026-04-21 21:13:24 +0100

    Integrate skim

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -4,8 +4,9 @@ return {
         lazy = false,     -- we don't want to lazy load VimTeX
         -- tag = "v2.15", -- uncomment to pin to a specific release
         init = function()
-            -- VimTeX configuration goes here, e.g.
-            vim.g.vimtex_view_method = "zathura"
+            vim.g.vimtex_view_method = "skim"
+            vim.g.vimtex_view_skim_sync = 1
+            vim.g.vimtex_view_skim_activate = 1
         end
     }
 }
