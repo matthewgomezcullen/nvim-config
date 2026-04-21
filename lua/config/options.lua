0a0bca750df539c9e82c0430e1799a34f0099658
Matthew Gomez Cullen <101350485+matthewgomezcullen@users.noreply.github.com>
2026-04-21 18:33:30 +0100

    Initial nvim config

---
+++ lua/config/options.lua
@@ -0,0 +1,5 @@
+vim.opt.number = true           -- line numbers 
+vim.opt.relativenumber = true   -- relative line numbers
+vim.opt.expandtab = true        -- use spaces instead of tabs
+vim.opt.tabstop = 4             -- how wide a tab looks
+vim.opt.shiftwidth = 4          -- indent size
