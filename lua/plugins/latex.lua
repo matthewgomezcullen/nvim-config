835f71335b90b27809e1057be6c28be88bd9148f
Matthew Gomez Cullen <101350485+matthewgomezcullen@users.noreply.github.com>
2026-07-10 17:19:20 +0100

    Use <leader>l instead of <localleader>l for VimTeX mappings

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -4,6 +4,7 @@ return {
         lazy = false,     -- we don't want to lazy load VimTeX
         -- tag = "v2.15", -- uncomment to pin to a specific release
         init = function()
+            vim.g.vimtex_mappings_prefix = "<leader>l"
             vim.g.vimtex_view_method = "skim"
             vim.g.vimtex_view_skim_sync = 1
             vim.g.vimtex_view_skim_activate = 1
